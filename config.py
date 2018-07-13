49abe2f6ec6be6d35007da0c74f8091bdae8250f
Kalsoom Malik <kalsoomalik09@gmail.com>
2018-07-13 16:35:34 -0400

    deleting keys

--- config.py
+++
@@ -1,6 +0,0 @@
-# API Keys
-
-consumer_key = "W1ox8FhHl7yZa4aScyTaKIvAB"
-consumer_secret = "lqFLq0feOvOo2fUdD48mIzi3QYM7lny2JHX4VCO3rMpwcOYnXR"
-access_token = "1013065777497427969-vQQM6Ga61NbuepOWChJsewMfbOn90v"
-access_token_secret = "ySksARDrSJAIRj0j6yEajBc664JJaI4h38ufVR9Et4p3O"
